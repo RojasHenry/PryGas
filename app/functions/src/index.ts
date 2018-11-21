bc2a627ca4002adb09e56481023d5cffa2647795
RojasHenry <henry.red1@hotmail.com>
2018-11-21 18:03:24 -0500

    Pedido mejorado notificaciones user-distri

--- app/functions/src/index.ts
+++ app/functions/src/index.ts
@@ -11,22 +11,83 @@ admin.initializeApp();
 //  response.send("Hello from Firebase!");
 // });
 
+exports.orderIsAccepted = functions.firestore
+    .document('orderGas/Norte/pedidos/{pedidoID}')
+    .onUpdate(async event =>{
+        const dataOrderbefore = event.before.data();
+        const dataOrderAfter = event.after.data();
+
+        const db = admin.firestore()
+
+        const distriRef = db.collection('users')
+
+        const distri = await distriRef.get();
+
+        var distriname = ""
+
+        distri.forEach(result =>{
+            if(result.id == dataOrderAfter.acceptedBy){
+                distriname += (result.data().lastname != "")? result.data().name+" "+result.data().lastname : result.data().name
+            }
+        })
+
+        // Notification content
+        const payload = {
+            notification: {
+                title: `Pedido Aceptado`,
+                body: `Tu pedido fue aceptado por ${distriname}`,
+                icon: 'https://goo.gl/Fz9nrQ'
+            }
+        }
+
+        const devicesRef = db.collection('devices').where("","==",dataOrderAfter.userUid)
+
+        const devices = await devicesRef.get();
+
+        const tokens = [];
+
+        // send a notification to each device token
+        devices.forEach(result => {
+            const token = result.data().token;
+            tokens.push(token)
+        })
+
+        return admin.messaging().sendToDevice(tokens, payload)
+    })
+
 exports.newOrderGasNorth = functions.firestore
     .document('orderGas/Norte/pedidos/{pedidoID}')
     .onCreate(async event =>{
         const dataOrder = event.data();
         console.log(JSON.stringify(dataOrder))
+
+        // ref to the device collection for the user
+        const db = admin.firestore()
+        //db.settings({ timestampsInSnapshots: true })
+
+        const userRef = db.collection('users')
+
+        const user = await userRef.get();
+
+        var username = ""
+
+        user.forEach(result =>{
+            if(result.id == dataOrder.userUid){
+                username += (result.data().lastname != "")? result.data().name+" "+result.data().lastname : result.data().name
+            }
+        })
+
+        console.log(username)
         // Notification content
         const payload = {
             notification: {
-                title: 'Nuevo Pedido',
-                body: `Tienes un nuevo Pedido`,
+                title: `Nuevo Pedido de Gas en Sector ${dataOrder.zone}`,
+                body: `Tienes un nuevo Pedido de ${username}`,
                 icon: 'https://goo.gl/Fz9nrQ'
             }
         }
 
-        // ref to the device collection for the user
-        const db = admin.firestore()
+        
         const distribuitorRef = db.collection('distributor').where('zone', "==",dataOrder.zone)
 
         const devicesRef = db.collection('devices')
